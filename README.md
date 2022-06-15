c69f04b9068814050f5e3894bca0b988c182bded
Michael <michael.miles2468@gmail.com>
2022-06-15 19:08:41 -0400

    filter functionality added to organizers events page. needs to be debugged

--- README.md
+++ README.md
@@ -3,18 +3,25 @@
 Welcome to APB, Asbury Park Bulletin!
 
 APB is built using a stack:
+
     -Rails(backend)
+
     -React.js(frontend)
+
         *React-big-calendar
+        
         *React-bootstrap
         *Action mailer
 
 If you would like to run APB please fork and clone this repositroy to your local machine.
-Open a terminal window and cd into the project folder.
-Next run the following commands:
-    *$rails s
 
-    *$npm start --prefix client
+Open a terminal window and cd into the project folder.
+
+Next run the following commands:
+
+    $rails s
+
+    $npm start --prefix client
 
 
 
--- client/src/components/OrganizedEventsPage.js
+++ client/src/components/OrganizedEventsPage.js
@@ -3,16 +3,27 @@ import { useNavigate } from 'react-router-dom'
 import EachEventCard from './EachEventCard'
 import {Modal} from 'react-bootstrap'
 import EditEventModal from './EditEventModal'
+import { set } from 'date-fns'
 
 
 function OrganizedEventsPage ({currentOrganizer}) {
     const [eventPageInfo, setEventPageInfo]=useState([])
     const [eventToEdit, setEventToEdit]=useState(undefined)
     const [stateToRerender, setStateToRerender]=useState(false)
+    const [filterState, setFilterState]=useState()
+
+    let filteredArray = []
     
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
+
+    function selectFilter (e) {
+        console.log(e)
+        setFilterState(e.target.value)
+    }
+
+    
 
     useEffect(()=>{
         fetch('/town_event_page')
@@ -25,10 +36,67 @@ function OrganizedEventsPage ({currentOrganizer}) {
     }, [stateToRerender])
     console.log(eventPageInfo)
 
+    function filterFunction (e) {
+        setFilterState(e.target.value)
+
+        if (filterState === 'start_time') {
+            eventPageInfo.sort(function(a, b){
+                let adate = a.start_time
+                let bdate = b.start_time
+
+                if (adate < bdate) {
+                    return -1;
+                }
+                if (adate > bdate) {
+                    return 1;
+                }
+                return 0;
+            })
+        } else if (filterState === 'type_of') {
+            eventPageInfo.sort(function(a, b){
+                let atype = a.type_of
+                let btype = b.type_of
+
+                if (atype < btype) {
+                    return -1
+                }
+                if (atype > btype) {
+                    return 1
+                }
+                return 0;
+            })
+        } else if (filterState === 'rsvps') {
+            eventPageInfo.sort(function(a, b){
+            let arsvps = a.signups.length
+            let brsvps = b.signups.length
+
+            if (arsvps > brsvps) {
+                return -1
+            }
+            if (arsvps > brsvps) {
+                return 1
+            }
+            return 0;
+        })
+        }
+    }
+
     return (
         <div className='organized-events-page'>
             <br></br>
             <h1>Your Upcoming events</h1>
+            <br></br>
+            <label htmlFor='filter'>Filter:</label>
+
+            <select
+            type='select'
+            name='fiter'
+            onChange={filterFunction}
+            >
+                <option value='start_time'>Date</option>
+                <option value='type_of'>Event Type</option>
+                <option value='rsvps'>RSVPs</option>
+            </select>
             <div className='each-event-card'>
                 {eventPageInfo.map((eachEvent)=>{
                     return(
